c51596ffe70c746c26533ad013e9310bb95c8436
monkey-krk <monkey@monkey.com.hr>
2014-09-29 11:53:48 +0200

    Header: Search

--- PurchasingTool/Assets/Js/Shared.js
+++ PurchasingTool/Assets/Js/Shared.js
@@ -1,4 +1,7 @@
-﻿$(document).ready(function () {
+﻿/// <reference path="../External/jQuery/jquery-2.1.1.min.js" />
+
+$(document).ready(function () {
+
 
     $('.dropdown-menu').on('click', function (e) {
         if ($(this).hasClass('dropdown-menu-form')) {
@@ -6,8 +9,49 @@
         }
     });
 
+
     if ($(".TooltipHolder").length > 0) {
         $(".TooltipHolder").tooltip();
     }
     
+
+    if ($(".TopSearch").length > 0) {
+
+        $(".TopSearchOpen").click(function (e) {
+            $(this).parent().find(".TopSearchBox").addClass("active");
+            $(this).hide();
+            e.preventDefault();
+        })
+        $(".TopSearchClose").click(function (e) {
+            $(this).parent().removeClass("active");
+            $(".TopSearchOpen").show();
+            e.preventDefault();
+        })
+
+        var NameList = [{ value: 'Adidas Runner 750mx' }, { value: 'Adidas Runner 620rx' }, { value: 'Nike Sport 78654' }, { value: 'Rebook black XL' }, { value: 'Apple Iphone 16Gb' }, { value: 'Apple Iphone 32Gb' }, { value: 'Apple Iphone 64Gb' }];
+        $(".TopSearch").autocomplete({
+            lookup: NameList,
+            onSelect: function (suggestion) {
+                var SelectedValue = suggestion.value;
+            }
+        });
+
+        $(".TopSearchGoBtn").click(function (e) {
+            SearchFor();
+            e.preventDefault();
+        });
+
+        $(".TopSearch").keypress(function (e) {
+            if (e.which == 13) {
+                SearchFor();
+            }
+        });
+
+        function SearchFor() {
+            alert("Search for : " + $(".TopSearch").val())
+        }
+
+    }
+
+
 });
