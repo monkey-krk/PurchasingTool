ca16fb68af6a4959e8fd853308b04572109912be
monkey-krk <monkey@monkey.com.hr>
2014-09-30 05:42:40 +0200

    Added Front Landing page

---
+++ Front/Assets/Js/Shared.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../External/jQuery/jquery-2.1.1.min.js" />
+
+$(document).ready(function () {
+
+    //Header transform
+    $(function () {
+        $('.MainHeader').data('size', 'big');
+    });
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 0) {
+            if ($('.MainHeader').data('size') == 'big') {
+                $('.MainHeader').data('size', 'small');
+                $('.MainHeader').fadeOut(300, function () {
+                    $('.MainHeader').addClass("sticky");
+                    $('.MainHeader').fadeIn(300);
+                });
+            }
+        }
+        else {
+            if ($('.MainHeader').data('size') == 'small') {
+                $('.MainHeader').data('size', 'big');
+                $('.MainHeader').fadeOut(300, function () {
+                    $('.MainHeader').removeClass("sticky");
+                    $('.MainHeader').fadeIn(300);
+                });
+            }
+        }
+    });
+
+
+
+    
+
+
+
+});
